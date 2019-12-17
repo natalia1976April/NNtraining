36bf3b1066f5bc40d24dc056861db12e60eb1d75
natasha <nkolpakova1976@mail.ru>
2019-12-17 11:24:49 +0300

    Task 15

--- addressbook-web-tests/addressbook-web-tests/tests/NewContact.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/NewContact.cs
@@ -4,6 +4,12 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.CSharp;
 
 
 namespace addressbook_web_tests
@@ -20,10 +26,9 @@ namespace addressbook_web_tests
 
             }
             return contacts;
-
         }
 
-        [Test, TestCaseSource("RandomContactDataProvider")]
+        [Test, TestCaseSource("ContactDataFromXmlFile")]
         public void TheNewContactTest(ContactData contact)
         {
 //            ContactData contact = new ContactData("Natalia", "Kolpakova");
@@ -47,7 +52,19 @@ namespace addressbook_web_tests
             {
                 Assert.AreNotEqual(contact.Id, toBeAdded.Id);
             }
+        }
 
+        public static IEnumerable<ContactData> ContactDataFromXmlFile()
+        {
+            List<ContactData> contacts = new List<ContactData>();
+            return (List<ContactData>)
+                new XmlSerializer(typeof(List<ContactData>)).Deserialize(new StreamReader(@"contacts.xml"));
+        }
+
+        public static IEnumerable<ContactData> ContactDataFromJsonFile()
+        {
+            List<ContactData> contacts = new List<ContactData>();
+            return JsonConvert.DeserializeObject<List<ContactData>>(File.ReadAllText(@"contacts.json"));
         }
     }
 }
